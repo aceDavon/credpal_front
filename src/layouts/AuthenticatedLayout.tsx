71a30fd8fc795f18e590561ba6a92a19dae2d066
aceDavon <makojidavid@gmail.com>
2025-04-07 15:16:33 +0100

    Fix build error with imports

--- src/layouts/AuthenticatedLayout.tsx
+++ src/layouts/AuthenticatedLayout.tsx
@@ -1,7 +1,7 @@
 import { Outlet, useNavigate } from "react-router-dom"
 import { useEffect, useState } from "react"
 import { useAuth } from "../context/AuthContext"
-import Navbar from "../components/NavBar"
+import Navbar from "../components/Navbar"
 
 export default function AuthenticatedLayout() {
   const { user, logout } = useAuth()
